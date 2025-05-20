c3cad8ad6d231e5210545d42fb3f0c53c3766665
vsssksvss <madnoja28@gmail.com>
2025-05-20 20:26:49 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,33 @@
 import java.util.Scanner;
 
-interface Calculator{
+interface Calculator {
     int add(int a, int b);
-    default int multi(int a, int b){
+    default int multi(int a, int b) {
         return a * b;
     }
-    static int subtract(int a, int b){
+    static int subtract(int a, int b) {
         return a - b;
     }
 }
 
-class Calculate implements Calculator{
+class Calculate implements Calculator {
     @Override
-    public int add(int a, int b){
+    public int add(int a, int b) {
         return a + b;
     }
 }
 
-
-
-
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("숫자 두개를 입력하세요: ");
+        System.out.println("Enter two numbers: ");
         int a = sc.nextInt();
         int b = sc.nextInt();
 
         Calculator math = new Calculate();
-        System.out.println("숫자: " + a + ", " + b + "\n============");
-        System.out.println("덧셈: " + math.add(a, b));
-        System.out.println("곱셈: " + math.multi(a, b));
-        System.out.println("뺼셈: " + Calculator.subtract(a, b));
+        System.out.println("Numbers: " + a + ", " + b + "\n============");
+        System.out.println("Addition: " + math.add(a, b));
+        System.out.println("Multiplication: " + math.multi(a, b));
+        System.out.println("Subtraction: " + Calculator.subtract(a, b));
     }
 }
